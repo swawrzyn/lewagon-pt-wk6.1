3a0d6645a02e45bc2ec188810115b29ac270cd95
Stefan Wawrzyn <swawrzyn@gmail.com>
2020-07-28 17:20:38 +0800

    init commit.

---
+++ pages/profile/index.js
@@ -0,0 +1,81 @@
+// pages/profile/index.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    currentUser: null,
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    const app = getApp();
+
+    console.log('userData', app.globalData.userInfo);
+
+  },
+  userInfoHandler(data) {
+    const app = getApp();
+    wx.BaaS.auth.loginWithWechat(data).then(user => {
+      console.log('user', user);
+        app.globalData.userInfo = user;
+        wx.setStorageSync('userInfo', user);
+        this.setData({
+          currentUser: user,
+        })
+      }, err => {
+        // **err 有两种情况**：用户拒绝授权，HError 对象上会包含基本用户信息：id、openid、unionid；其他类型的错误，如网络断开、请求超时等，将返回 HError 对象（详情见下方注解）
+    })
+  },
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
